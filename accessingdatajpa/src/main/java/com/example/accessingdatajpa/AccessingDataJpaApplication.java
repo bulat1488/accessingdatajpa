943b99df0af6e1b34ee52a64dc7e57b67989246d
bulat1488 <bulat6688@gmail.com>
2022-05-27 15:17:28 +0300

    DataJpa class

--- accessingdatajpa/src/main/java/com/example/accessingdatajpa/AccessingDataJpaApplication.java
+++ accessingdatajpa/src/main/java/com/example/accessingdatajpa/AccessingDataJpaApplication.java
@@ -17,7 +17,7 @@ public class AccessingDataJpaApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demoOrder(ClickOrderRepository clickOrderRepository) {
+	public CommandLineRunner demoOrder(ClickOrderRepository clickOrderRepository, CustomerRepository repository) {
 		return (args) -> {
 			// save a few customers
 			clickOrderRepository.save(new ClickOrders("sneakers", "super","15.05.2021 12:08"));
@@ -25,6 +25,11 @@ public class AccessingDataJpaApplication {
 			clickOrderRepository.save(new ClickOrders("shorts", "++++++!!!","29.11.2021 09:18"));
 			clickOrderRepository.save(new ClickOrders("swimsuit", "very open","06.01.2022 11:11"));
 			clickOrderRepository.save(new ClickOrders("track suit", "not a high-quality suit","12.04.2021 16:15"));
+			repository.save(new Customer("Jack", "Bauer"));
+			repository.save(new Customer("Chloe", "O'Brian"));
+			repository.save(new Customer("Kim", "Bauer"));
+			repository.save(new Customer("David", "Palmer"));
+			repository.save(new Customer("Michelle", "Dessler"));
 //			repository.save(new Customer("Jack", "Bauer"));
 //			repository.save(new Customer("Chloe", "O'Brian"));
 //			repository.save(new Customer("Kim", "Bauer"));
@@ -39,6 +44,13 @@ public class AccessingDataJpaApplication {
 			}
 			log.info("");
 
+			log.info("Customer found with findAll():");
+			log.info("-------------------------------");
+			for (Customer customer : repository.findAll()) {
+				log.info(customer.toString());
+			}
+			log.info("");
+
 			// fetch an individual customer by ID
 			ClickOrders clickOrders = clickOrderRepository.findById(1L);
 			log.info("Orders found with findById(1L):");
@@ -46,17 +58,65 @@ public class AccessingDataJpaApplication {
 			log.info(clickOrders.toString());
 			log.info("");
 
+			Customer customer = repository.findById(8L);
+			log.info("Orders found with findById(1L):");
+			log.info("--------------------------------");
+			log.info(customer.toString());
+			log.info("");
+
 			// fetch customers by last name
 			log.info("Orders found with findValue('shorts'):");
 			log.info("--------------------------------------------");
-			clickOrderRepository.findByValue("shorts").forEach(bauer -> {
-				log.info(bauer.toString());
+			clickOrderRepository.findByValue("shorts").forEach(shorts -> {
+				log.info(shorts.toString());
+
 			});
-			// for (Customer bauer : repository.findByLastName("Bauer")) {
-			// 	log.info(bauer.toString());
-			// }
+
+			log.info("Orders found with findValue('Bauer'):");
+			log.info("--------------------------------------------");
+			repository.findByLastName("Bauer").forEach(bauer -> {
+				log.info(bauer.toString());
+
+			});
 			log.info("");
 		};
 	}
+
+//	public CommandLineRunner demoCustomer(CustomerRepository repository) {
+//		return (args) -> {
+//			// save a few customers
+//			repository.save(new Customer("Jack", "Bauer"));
+//			repository.save(new Customer("Chloe", "O'Brian"));
+//			repository.save(new Customer("Kim", "Bauer"));
+//			repository.save(new Customer("David", "Palmer"));
+//			repository.save(new Customer("Michelle", "Dessler"));
+//
+//			// fetch all customers
+//			log.info("Customers found with findAll():");
+//			log.info("-------------------------------");
+//			for (Customer customer : repository.findAll()) {
+//				log.info(customer.toString());
+//			}
+//			log.info("");
+//
+//			// fetch an individual customer by ID
+//			Customer customer = repository.findById(1L);
+//			log.info("Customer found with findById(1L):");
+//			log.info("--------------------------------");
+//			log.info(customer.toString());
+//			log.info("");
+//
+//			// fetch customers by last name
+//			log.info("Customer found with findByLastName('Bauer'):");
+//			log.info("--------------------------------------------");
+//			repository.findByLastName("Bauer").forEach(bauer -> {
+//				log.info(bauer.toString());
+//			});
+//			// for (Customer bauer : repository.findByLastName("Bauer")) {
+//			// 	log.info(bauer.toString());
+//			// }
+//			log.info("");
+//		};
+//	}
 
 }
